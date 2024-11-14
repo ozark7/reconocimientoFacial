ec78f7479806fb49f026fbec5a8637e41f48e9bb
ozarpepi <gotbd7@gmil.com>
2024-11-14 14:35:21 -0500

    firstCommit

---
+++ app/reconocimientoScript.py
@@ -0,0 +1,55 @@
+import cv2
+import os
+
+dataPath = 'D:/UPC2024-2/IA/2 corte/reconocimientoFacial/data' #Cambia a la ruta donde hayas almacenado Data
+imagePaths = os.listdir(dataPath)
+print('imagePaths=',imagePaths)
+
+#reconocimiento de rostro
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Leyendo el modelo 
+face_recognizer.read('D:/UPC2024-2/IA/2 corte/reconocimientoFacial/modeloLBPHFace.xml')
+
+
+#============================================================================
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)#VIDEO CAMARA
+
+#cap = cv2.VideoCapture('D:/UPC2024-2/IA/2 corte/reconocimientoFacial/users/pepi.mp4')#USUARIOS EXISTENTES
+
+#cap = cv2.VideoCapture('D:/UPC2024-2/IA/2 corte/reconocimientoFacial/test/break.MP4')#PRUEBA
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+while True:
+	ret,frame = cap.read()
+	if ret == False: break
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	auxFrame = gray.copy()
+
+	faces = faceClassif.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+
+
+	for (x,y,w,h) in faces:
+		rostro = auxFrame[y:y+h,x:x+w]
+		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+		result = face_recognizer.predict(rostro)
+
+		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+		#cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		
+		# LBPHFace
+		if result[1] < 60:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		
+	cv2.imshow('frame',frame)
+	k = cv2.waitKey(1)
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
